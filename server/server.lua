2b352c94ad930d0b4d8de094e73f75dee03207d5
matteo0003 <angoin.matteo@gmail.com>
2024-11-03 17:08:53 +0100

    Update

--- server/server.lua
+++ server/server.lua
@@ -1,4 +1,4 @@
 RegisterCommand("disarm", function(source, args, _)
 	local player <const> = args[1] or source
-	TriggerServerEvent("ox_inventory:disarm", player, false)
+	TriggerClientEvent("ox_inventory:disarm", player, false)
 end)
